564f12d33e91bc76a035b20c737cad55084e0823
MuriloPereiraDEV <102379940+MuriloPereiraDEV@users.noreply.github.com>
2022-10-21 22:53:08 -0300

    Comments

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 from flask import request, Flask, render_template
 from data import dados
 
+#TRANSLATE LANGUAGE INFORMATION
 def langaugeTrans(language):
     padrao = "Idioma: "
     if language == "pt":
@@ -30,6 +31,8 @@ def langaugeTrans(language):
     elif language == "zh-TW":
         return padrao + "Chinês - Tradicional"
 
+
+#TRANSLATES FEELING INFORMATION
 def sentimentTrans(sentiment):
     padrao = "Sentimento Geral: "
     if sentiment == "POSITIVE":
@@ -41,16 +44,20 @@ def sentimentTrans(sentiment):
     elif sentiment == "MIXED":
         return padrao + "Misto"
 
+#IDENTIFIES LANGUAGE AND FEELING
 def sentimentAnalisis(txt):
 
+    #CONNECT WITH AMAZON COMPREHEND SERVICE
     comprehend = boto3.client(service_name='comprehend', region_name=dados.REGION_NAME, aws_access_key_id=dados.AWS_ACCESS_KEY_ID, aws_secret_access_key=dados.AWS_SECRET_ACCESS_KEY)
 
+    #DETECT THE LANGUAGE OF THE TEXT
     lan = json.dumps(comprehend.detect_dominant_language(Text = txt), sort_keys=True, indent=4)
     
     lanAux = json.loads(lan)
     lanList = list(lanAux.items())
     languageTxtUser = lanList[0][1][0]['LanguageCode']
 
+    #DETECTS THE FEELING OF THE TEXT
     sentiment = json.dumps(comprehend.detect_sentiment(Text=txt, LanguageCode=str(languageTxtUser)), sort_keys=True, indent=4)
     sentimentAux = json.loads(sentiment)
     sentimentList = list(sentimentAux.items())
@@ -61,16 +68,19 @@ def sentimentAnalisis(txt):
 
 app = Flask(__name__,template_folder='templates')
 
+#SET THE TEMPLATE FOR THE DEFAULT ROUTE
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#SET THE TEMPLATE TO THE SPECIFIED ROUTE
 @app.route('/test', methods=['POST'])
 def test():
     output = request.get_json() 
     result = json.loads(output)
     languageTxtUser, sentimentTxtUser = sentimentAnalisis(result['txtUser'])
 
+    #ANALYZE IF THE ENTRY IS VALID
     if (languageTxtUser == '') & (sentimentTxtUser == ''):
         return ['Falha na compreensão do idioma. Tente novamente.', '']
         
@@ -83,7 +93,6 @@ def test():
     return info
 
 
+#EXECUTE MAIN FUNCTION
 if __name__ == "__main__":
     app.run(debug=True)
-
-#https://softexsentimentanalysis.herokuapp.com/
